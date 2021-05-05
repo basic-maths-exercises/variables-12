3cd1be6876029a98e46c0e5a18749c6fdffab8ab
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-05 15:58:41 +0100

    Fixed exercise

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 xvals = np.zeros(100)
-xvals[0], xvals[1] = 1, 2
-fibonacci = np.ones(100)
+fibonacci = np.zeros(100)
 # Insert your code for the fibonacci series here
-for i in range(2,100) :
-  xvals[i]=i+1
-  fibonacci[i] = fibonacci[i-2] + fibonacci[i-1]
+
+
 
 plt.plot( xvals, fibonacci, 'ko' )
 plt.xlabel("Index")
