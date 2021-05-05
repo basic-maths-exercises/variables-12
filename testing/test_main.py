4d438517bd90da9f0b9927c4c49ce4de502d8825
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-05 15:35:28 +0100

    Fixed test

--- testing/test_main.py
+++ testing/test_main.py
@@ -12,7 +12,6 @@ from main import *
 
 class UnitTests(unittest.TestCase) :
     def test_fib(self) :
-       fib=np.zeros(100)
-       fib[0], fib[1] = 1, 1 
-       for i in range(2,100) : vv = fib[i-2] + fib[i-1]
+       fib=np.ones(100)
+       for i in range(2,100) : fib[i] = fib[i-2] + fib[i-1]
        assert( vc.check_vars("fibonacci",fib) )    
